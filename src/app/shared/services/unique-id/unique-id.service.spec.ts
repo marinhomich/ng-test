fed4713880dbefc4845f4a552edb277f94aa6671
Michel Marinho <marinhomich@gmail.com>
2024-10-28 09:59:52 -0400

    test: unique-id.service

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,16 +1,43 @@
 import { UniqueIdService } from "./unique-id.service";
 
 describe(UniqueIdService.name, () => {
-  var a: boolean;
+
+  let service: UniqueIdService;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  })
 
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app')
-    expect(id).toContain('app-')
+    expect(id.startsWith('app-')).toBe(true)
   })
 
-  it('Segunda Condição que queremos testar', () => {
-    a = true
-    expect(a).toBe(true)
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate ids when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    
+    expect(ids.size).toBe(50);
   })
+
+  it(`#${UniqueIdService.prototype.getNumberofGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+
+    expect(service.getNumberofGeneratedUniqueIds()).toBe(2)
+  })
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should throw when called empty`, () => {
+
+    const emptyValues = ["", null, undefined];
+
+    emptyValues.forEach(emptyValue => {
+      expect(() => service.generateUniqueIdWithPrefix(emptyValue as any)).withContext(`Empty Value: ${emptyValue}`).toThrow();
+    })
+
+  })
+
+  
+
 })
